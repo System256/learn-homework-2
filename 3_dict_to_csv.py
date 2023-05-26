959316f08c0692e8dcd2ac673ed5529ba6401ac6
Roman <system256@ya.ru>
2023-05-27 00:23:51 +0300

    done

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,3 +1,5 @@
+import csv
+
 """
 
 Домашнее задание №2
@@ -10,12 +12,20 @@
 
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+def list_to_csv():
+    employees_list = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'}
+        ]
+
+    with open('export.csv', 'w', encoding='utf-8', newline='') as f:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for employee in employees_list:
+            writer.writerow(employee)
+
 
 if __name__ == "__main__":
-    main()
+    list_to_csv()
